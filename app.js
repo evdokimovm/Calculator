50315a379811684ca0d6404fe102499370044e2f
evdokimovm <tel89147925532@gmail.com>
2016-07-12 00:42:37 +1100

    Calculator

---
+++ app.js
@@ -0,0 +1,33 @@
+var textField = document.getElementById('textField');
+textField.focus();
+var submit = document.getElementById('submitButton').addEventListener('click', buttonClicked, false);
+
+function buttonClicked(e) {
+	e.preventDefault();
+	x = textField.value;
+	try {
+		x = eval(x);
+		textField.value = x;
+	} catch (ex) {
+		alert('Wrong Format');
+	}
+}
+
+function addToField(n) {
+	textField.value += n;
+	textField.focus();
+}
+
+function power(y) {
+	x = textField.value;
+	x = Math.pow(x, y);
+	textField.value = x;
+	textField.focus();
+}
+
+function powten() {
+	x = textField.value;
+	x = Math.pow(10, x);
+	textField.value = x;
+	textField.focus();
+}
